0525516f7b80774365f9f4f4a2416ced738b60b3
Vitali Pomanitski <vitali.pom@gmail.com>
2020-12-26 17:28:14 +0200

    added second page (facebook chat)

---
+++ text_appearance.js
@@ -0,0 +1,9 @@
+
+function showTextLine(){	 
+	 $('#phone_image').append('<div id="underline_image2" style="z-index:0;" class="font_size padding textbox left_textbox">hi</div>');
+	 $('#phone_image').append('<div id="underline_image3" style="z-index:0;" class="font_size padding textbox right_textbox">hello</div>');
+	 $('#phone_image').append('<div id="underline_image4" style="z-index:0;" class="font_size padding textbox left_textbox">how are you doin?</div>');
+	 $('#phone_image').append('<div id="underline_image5" style="z-index:0;" class="font_size padding textbox right_textbox">fine, how are u?</div>');
+ 	 	
+	
+}
