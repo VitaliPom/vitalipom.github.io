eabf93b4676978e56e30ea5cfe2b2b1cbe915f26
Vitali Pomanitski <vitali.pom@gmail.com>
2020-12-27 19:47:00 +0200

    css split

---
+++ js/butterflies.js
@@ -0,0 +1,17 @@
+for(var i = 1;  i <= 10; i++){
+    $("#butterflies").append('\
+        <div class="borboletas-'+i+'">\
+            <div class="borboleta-'+i+'">\
+                <div class="borboleta-oval-squish-'+i+'">\
+                    <div class="borboleta-oval-'+i+'">\
+                        <div class="borboleta-radial-'+i+'">\
+                            <div class="borboleta-gfx-'+i+'">\
+                                <div class="borboleta-anim-'+i+'"></div>\
+                            </div>\
+                        </div>\
+                    </div>\
+                </div>\
+            </div>\
+        </div>\
+    ');
+}
